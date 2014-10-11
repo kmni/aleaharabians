c0fdefe1d0be0cf2499adffad5fc754b40307590
Tomas Musiol <tomas.musiol@gmail.com>
2014-10-11 18:55:01 +0200

    Javascript library and Slider at homepage

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,4 +1,60 @@
 
 $(document).ready(function() {
 
+// fancybox
+// ====================================
+
+$(".fancybox").fancybox({
+    'transitionIn'  :   'elastic',
+    'transitionOut' :   'elastic',
+    'speedIn'       :   600,
+    'speedOut'      :   200,
+    'overlayShow'   :   false,
+    'padding'       :   0
+});
+
+// fancybox video
+// ====================================
+
+$(".fancybox-video").click(function() {
+    $.fancybox({
+        'transitionIn'  :   'elastic',
+        'transitionOut' :   'elastic',
+        'speedIn'       :   600,
+        'speedOut'      :   200,
+        'overlayShow'   :   false,
+        'width'         :   640,
+        'height'        :   385,
+        'href'          :  this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+        'type'          :   'swf',
+        'swf'           :   { 'wmode': 'transparent', 'allowfullscreen': 'true' }
+    });
+    return false;
+});
+
+// slider at homepage
+// ====================================
+
+if( $(".js-slider").length > 0 ){
+    $('.js-slider').jcarousel({
+        wrap: 'circular'
+    });
+    $('.js-slider').jcarouselAutoscroll({
+        interval: 5000
+    });
+}
+
+// submenu
+// ====================================
+
+$(".menu__item.has-submenu").hover(
+    function(){
+        $(this).find(".submenu").fadeIn(200);
+    },
+    function(){
+        $(".submenu").fadeOut(200);
+    }
+);
+
+
 });
